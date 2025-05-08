89cd251f6cff3b1f490f82e8c5ede44383057dbb
praveenkumar4595-devops <kumarnani4595@gmail.com>
2025-05-08 16:36:32 +0530

    Update main.tf

--- praveen/main.tf
+++ praveen/main.tf
@@ -2,6 +2,15 @@ resource "aws_instance" "example" {
   ami           = "ami-085386e29e44dacd7"  # Amazon Linux 2
   instance_type = "t2.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "<h1>Welcome to Harness EC2 Instance</h1>" > /var/www/html/index.html
+              EOF
+
   tags = {
     Name = "Harness-EC2"
   }
