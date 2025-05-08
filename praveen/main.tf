0d3288b8daffb5f56193e608145bbaafc8b1b1fd
praveenkumar4595-devops <kumarnani4595@gmail.com>
2025-05-08 18:52:09 +0530

    Update main.tf

--- praveen/main.tf
+++ praveen/main.tf
@@ -1,6 +1,31 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP traffic"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "example" {
-  ami           = "ami-085386e29e44dacd7"  # Amazon Linux 2
-  instance_type = "t2.micro"
+  ami                    = "ami-085386e29e44dacd7" # Amazon Linux 2
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_http.id]
+  associate_public_ip_address = true
 
   user_data = <<-EOF
               #!/bin/bash
@@ -12,6 +37,6 @@ resource "aws_instance" "example" {
               EOF
 
   tags = {
-    Name = "Harness-EC2"
+    Name = "Harness-trail"
   }
 }
